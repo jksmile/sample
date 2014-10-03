bc2071d4efc01c92d61d5491e72b4625f86c1900
jk <542723205@qq.com>
2014-10-03 16:59:59 +0800

    'update'

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -7,37 +7,4 @@ public class UserService{
 
 
 
-    public void test(){
-    
-        System.out.print("OK");
-    }
-
-
-    public void love(){
-    
-        System.out.println("love");
-    }
-
-
-    public void mine(){
-    
-        System.out.println("OK");
-    }
-
-
-    public void maybe(){
-    
-        System.out.println("maybe");
-    }
-
-    public void life(){
-    
-    
-        System.out.println("life");
-    }
-
-    public void why(){
-        System.out.println("why");
-    }
-
 }
